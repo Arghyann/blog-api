d452325ff27b02155e97d8ca74f588110ad588c7
Aryan <aryanmane2016@gmail.com>
2026-09-25 16:40:39 +0530

    db.go

---
+++ models.go
@@ -0,0 +1,12 @@
+package main
+
+import "time"
+
+type Post struct {
+	ID          string    `json:"id"`
+	Slug        string    `json:"slug"`
+	Title       string    `json:"title"`
+	Body        string    `json:"body"`
+	Tags        string    `json:"tags"`
+	PublishedAt time.Time `json:"published_at"`
+}
